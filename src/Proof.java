7c97f6467b6fb20abe9ef270c95cee4d58f63fc2
CaioVitor1 <caiovitor33@hotmail.com>
2023-01-11 12:24:41 -0300

    feat: create class

---
+++ src/Proof.java
@@ -0,0 +1,13 @@
+public class Proof {
+    private String matter; 
+    private int numberOfQuestions;
+    private String teacher;
+
+    String getTeacher(){
+        return this.teacher;
+    }
+
+    int getNumberOfQuestions(){
+    return this.numberOfQuestions;
+    }
+    }
---
+++ src/Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+    private String name;
+    private String matter;
+    private int yearsOfExperience;
+    private int numberOfClasses;
+
+    String getMatter(){
+        return this.matter;
+    }
+}
