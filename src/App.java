0383a720f7d4ec56eb12162f5560f25afa5bc40f
CaioVitor1 <caiovitor33@hotmail.com>
2023-01-11 15:54:14 -0300

    feat: add App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Proof Proof = new Proof();
+       Teacher Teacher = new Teacher();
+
+       System.out.println(Teacher.getMatter());
+       System.out.println(Proof.getTeacher());
     }
 }
